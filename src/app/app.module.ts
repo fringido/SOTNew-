b817df5375fd01569f0a22961df02dc11c60a686
fringido <47158039+fringido@users.noreply.github.com>
2022-11-14 10:42:57 -0600

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {PasswordModule} from 'primeng/password';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -14,7 +13,6 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     NgbModule,
     NoopAnimationsModule,
-    PasswordModule
   ],
   providers: [],
   bootstrap: [AppComponent]
