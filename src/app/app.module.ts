603ae3c2df7f431b9c70a1ffc21d09d157ddaf1d
fringido <47158039+fringido@users.noreply.github.com>
2022-11-14 10:42:34 -0600

    [ADD] font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {PasswordModule} from 'primeng/password';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -12,7 +13,8 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     NgbModule,
-    NoopAnimationsModule
+    NoopAnimationsModule,
+    PasswordModule
   ],
   providers: [],
   bootstrap: [AppComponent]
