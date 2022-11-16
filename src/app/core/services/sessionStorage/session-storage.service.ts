bfe784be7fbca9c79bd8d0d884e7d1b4c99a3f6a
Gerardo Arturo Miranda <A01338074@itesm.mx>
2022-11-16 08:46:37 -0600

    sessionStorage service

--- src/app/core/services/sessionStorage/session-storage.service.ts
+++ src/app/core/services/sessionStorage/session-storage.service.ts
@@ -6,4 +6,22 @@ import { Injectable } from '@angular/core';
 export class SessionStorageService {
 
   constructor() { }
+
+  setItem<T>(key: string, value: T) {
+    sessionStorage.setItem(key, JSON.stringify(value));
+  }
+
+  getItem<T>(key: string): T | {} {
+    return JSON.parse(sessionStorage.getItem(key) || '{}');
+  }
+
+  setItems(obj: object) {
+    for (const [key, value] of Object.entries(obj)) {
+      this.setItem(key, value);
+    }
+  }
+
+  clear() {
+    sessionStorage.clear();
+  }
 }
