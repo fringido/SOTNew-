5d2794a6426dc93588a8c74e5d48449b201e672e
Gerardo Arturo Miranda <A01338074@itesm.mx>
2022-11-17 13:49:21 -0600

    renombra de app routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.routes';
+import { AppRoutingModule } from './app-routing.module';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -14,7 +13,6 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    NgbModule,
     NoopAnimationsModule,
     AppRoutingModule,
     GraphQLModule,
