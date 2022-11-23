091dad96c40d4d9419565e1569f8f19422fdd11d
Gerardo Arturo Miranda <A01338074@itesm.mx>
2022-11-23 16:56:42 -0600

    [ADD] home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -13,6 +13,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     NoopAnimationsModule,
     AppRoutingModule,
     GraphQLModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -46,6 +46,7 @@ export class LoginComponent implements OnInit {
 
   login() {
     console.log(this.loginForm.value)
+    this.router.navigate(['home'])
   }
 
   toggleShowPassword() {
