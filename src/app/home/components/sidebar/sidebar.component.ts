e2d34e8d1269b4b3b92faa042756244c1408b528
Gerardo Arturo Miranda <A01338074@itesm.mx>
2022-11-30 11:53:45 -0600

    [REFACTOR] se centralizaron los estilos de color de fondo y de color de texto en styles.scss

--- src/app/home/components/sidebar/sidebar.component.ts
+++ src/app/home/components/sidebar/sidebar.component.ts
@@ -14,20 +14,20 @@ export class SidebarComponent implements OnInit {
   isBurguerMenuSelected = false;
 
   roomTypes = [
-    { name: 'Libre', count: 10, color: 'success', icon: 'far fa-check-circle', isTextScrollable: false },
-    { name: 'Por cobrar', count: 10, color: 'success', icon: 'far fa-check-circle icon-cobrar', isTextScrollable: false },
-    { name: 'Preparada', count: 10, color: 'success', icon: 'far fa-check-circle icon-car', isTextScrollable: false },
-    { name: 'Ocupada', count: 10, color: 'danger', icon: 'far fa-check-circle icon-pareja-ocupada', isTextScrollable: false },
-    { name: 'Room service', count: 10, color: 'danger', icon: 'far fa-check-circle icon-roomservice fa-2xs', isTextScrollable: true },
-    { name: 'Sucia', count: 10, color: 'warn', icon: 'far fa-check-circle icon-sucia', isTextScrollable: false },
-    { name: 'Media Sucia', count: 10, color: 'orange', icon: 'far fa-check-circle icon-sucia', isTextScrollable: true },
-    { name: 'Limpieza', count: 10, color: 'blue', icon: 'far fa-check-circle icon-limpieza', isTextScrollable: false },
-    { name: 'Supervisión', count: 10, color: 'blue', icon: 'far fa-check-circle icon-search', isTextScrollable: false },
-    { name: 'Reservar', count: 10, color: 'purple', icon: 'icon-calendar', isTextScrollable: false },
-    { name: 'Preparada y Reservada', count: 10, color: 'purple', icon: 'icon-calendar', isTextScrollable: true },
-    { name: 'Bloqueada', count: 10, color: 'purple', icon: 'far fa-check-circle icon-candado', isTextScrollable: false },
-    { name: 'Mantenimiento', count: 10, color: 'grey-item', icon: 'far fa-check-circle icon-mantenimiento', isTextScrollable: true },
-    { name: 'Alertas', count: 10, color: 'grey-item', icon: 'fas fa-clock', isTextScrollable: false },
+    { name: 'Libre', count: 10, color: 'txt-success', icon: 'far fa-check-circle', isTextScrollable: false },
+    { name: 'Por cobrar', count: 10, color: 'txt-success', icon: 'far fa-check-circle icon-cobrar', isTextScrollable: false },
+    { name: 'Preparada', count: 10, color: 'txt-success', icon: 'far fa-check-circle icon-car', isTextScrollable: false },
+    { name: 'Ocupada', count: 10, color: 'txt-danger', icon: 'far fa-check-circle icon-pareja-ocupada', isTextScrollable: false },
+    { name: 'Room service', count: 10, color: 'txt-danger', icon: 'far fa-check-circle icon-roomservice fa-2xs', isTextScrollable: true },
+    { name: 'Sucia', count: 10, color: 'txt-warn', icon: 'far fa-check-circle icon-sucia', isTextScrollable: false },
+    { name: 'Media Sucia', count: 10, color: 'txt-orange', icon: 'far fa-check-circle icon-sucia', isTextScrollable: true },
+    { name: 'Limpieza', count: 10, color: 'txt-blue', icon: 'far fa-check-circle icon-limpieza', isTextScrollable: false },
+    { name: 'Supervisión', count: 10, color: 'txt-blue', icon: 'far fa-check-circle icon-search', isTextScrollable: false },
+    { name: 'Reservar', count: 10, color: 'txt-purple', icon: 'icon-calendar', isTextScrollable: false },
+    { name: 'Preparada y Reservada', count: 10, color: 'txt-purple', icon: 'icon-calendar', isTextScrollable: true },
+    { name: 'Bloqueada', count: 10, color: 'txt-purple', icon: 'far fa-check-circle icon-candado', isTextScrollable: false },
+    { name: 'Mantenimiento', count: 10, color: 'txt-grey-item', icon: 'far fa-check-circle icon-mantenimiento', isTextScrollable: true },
+    { name: 'Alertas', count: 10, color: 'txt-grey-item', icon: 'fas fa-clock', isTextScrollable: false },
   ]
 
   constructor(
