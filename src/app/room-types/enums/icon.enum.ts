af393639f2892a62ba902ef5e562df09ebfc7086
Gerardo Arturo Miranda <A01338074@itesm.mx>
2022-12-12 16:57:45 -0600

    [ADD] configuraciones extras para las habitaciones

--- src/app/room-types/enums/icon.enum.ts
+++ src/app/room-types/enums/icon.enum.ts
@@ -1,9 +1,11 @@
 
 export enum RoomIconsEnum {
     CHECK = 'far fa-check-circle',
-    CAR = 'far fa-check-circle icon-car',
-    PAREJA_OCUPADA = 'far fa-check-circle icon-pareja-ocupada',
-    SUCIA = 'far fa-check-circle icon-sucia',
-    LIMPIEZA = 'far fa-check-circle icon-limpieza',
-    SUPERVISION = 'far fa-check-circle icon-search'
+    CAR = 'far icon-car',
+    PAREJA_OCUPADA = 'far icon-pareja-ocupada',
+    SUCIA = 'far icon-sucia',
+    LIMPIEZA = 'far icon-limpieza',
+    SUPERVISION = 'far icon-search',
+    ROOM_SERVICE = 'far icon-rooms',
+    POR_COBRAR = 'far icon-cobrar'
 }
--- src/app/room-types/enums/room-status.enum.ts
+++ src/app/room-types/enums/room-status.enum.ts
@@ -6,5 +6,9 @@ export enum RoomStatusEnum {
     SUCIA = 'Sucia',
     MEDIA_SUCIA = 'Media Sucia',
     EN_LIMPIEZA = 'En limpieza',
-    EN_SUPERVISION  = 'En supervisión'
+    EN_SUPERVISION  = 'En supervisión',
+    OCUPADA_POR_COBRAR = 'Ocupada por cobrar',
+    OCUPADA_ROOM_SERVICE = 'Ocupada room service',
+    OCUPADA_ROOM_SERVICE_POR_COBRAR = 'Ocupada room service por cobrar',
+    OCUPADA_ROOM_SERVICE_EXTRAS_POR_COBRAR = 'Ocupada room service extras por cobrar'
 }
