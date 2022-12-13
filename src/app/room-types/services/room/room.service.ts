fd30b2e137d77c53bec01dffebc2b978bc8a9057
Gerardo Arturo Miranda <A01338074@itesm.mx>
2022-12-13 15:15:37 -0600

    Mensaje extra-entrada-pago realizado con exito

--- src/app/room-types/services/room/room.service.ts
+++ src/app/room-types/services/room/room.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Room } from '../../interfaces/room.interface';
 
 @Injectable()
 export class RoomService {
 
-  private selectedRoom = new BehaviorSubject<any>({});
+  private selectedRoom = new BehaviorSubject<Room | null>(null);
   selectedRoom$ = this.selectedRoom.asObservable();
 
   constructor(
