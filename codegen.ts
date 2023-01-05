d53c6e79f63a4d867bfb83cc2a54e49b404caeab
Max <mmaaxx77@msn.com>
2023-01-05 14:58:30 -0600

    pruebas

--- codegen.ts
+++ codegen.ts
@@ -1,12 +1,39 @@
 import type { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
- schema: 'http://my-graphql-api.com/graphql',
- documents: './src/**/*.ts',
- generates: {
- './graphql/generated.ts': {
- plugins: ['typescript', 'typescript-operations', 'typescript-apollo-angular']
- }
- }
+  overwrite: true,
+  schema: 'src/app/schema.graphql',
+  // generates: {
+  //   'src/app/core/graphQL/file': {
+  //     plugins: ['apollo-angular'],
+  //     config: {
+  //       apolloAngularVersion: 1
+  //     },
+  //   },
+  //   './schema.json': {
+  //     plugins: ['introspection'],
+  //   },
+  //   'src/app/core/modelsGenere/file.ts': {
+  //     plugins: ['typescript', 'typescript-operations', 'typescript-generic-sdk'],
+  //   },
+  // }
+  generates: {
+    './src/core/modules/': {
+      preset: 'graphql-modules',
+      presetConfig: {
+        baseTypesPath: '../generated-types/graphql.ts',
+        filename: 'generated-types/module-types.ts'
+      },
+      plugins: [
+        {
+          add: {
+            content: 'typescript-operations'
+          }
+        },
+        'typescript',
+        'typescript-resolvers'
+      ]
+    }
+  }
 }
 export default config
